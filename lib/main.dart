4669de54fc35a6abbcba138e635f817263edea40
Mysterio <azamatabduxalikov57@gmail.com>
2025-01-29 13:48:19 +0500

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true),
       home: const WeatherScreen(),
+    //   xksanjjx
     );
   }
 }
